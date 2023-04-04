0472ca8e2f79b5d977fd055c234e181108ba6add
keenlight96 <nguyenkimlong29196@gmail.com>
2023-04-04 14:55:06 +0700

    id=15396

---
+++ Bai7.2.js
@@ -0,0 +1,6 @@
+let width = prompt("Nhập chiều rộng: ");
+let height = prompt("Nhập chiều cao: ");
+let widthCal = parseInt(width);
+let heightCal = parseInt(height);
+let areaCal = widthCal * heightCal;
+document.write("Diện tích: " + areaCal);
